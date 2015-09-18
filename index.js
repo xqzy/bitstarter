23522e2f290dbe5caf71f927319cb7521da7a096
Rob Schuurman <rob.schuurman@hccnet.nl>
2015-09-18 12:54:36 +0000

    index.js fixed ..

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   var buffer = new Buffer(100);
   buffer = fs.readFileSync('index.html');
-     
+
   response.send(buffer.toString('ascii'));
-})
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
